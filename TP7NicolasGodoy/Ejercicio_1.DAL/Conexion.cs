1086d4eedc1e26b1d95ff96132b14740efff4f22
nicolasgodoy <nicolasgodoy540@gmail.com>
2025-02-07 20:18:51 -0300

    cambios

--- TP7NicolasGodoy/Ejercicio_1.DAL/Conexion.cs
+++ TP7NicolasGodoy/Ejercicio_1.DAL/Conexion.cs
@@ -14,7 +14,7 @@ namespace Ejercicio_1.DAL
             {
             //con = new SqlConnection("Server=(localdb)\\mssqllocaldb;initial catalog=BasePruebaAngular;integrated security=true"); // PARA TRABAJAR LOCAL
             //con = new SqlConnection("Data Source=CrudAngularDB.mssql.somee.com;Initial Catalog=CrudAngularDB;user id=usser123_SQLLogin_1;pwd=j8vgakdrn6;TrustServerCertificate=True;"); SERVIDOR SE VOLVIO PAGO
-            con = new SqlConnection("Data Source=SQL1002.site4now.net;Initial Catalog=db_ab28c4_basepruebaangular;User Id=db_ab28c4_basepruebaangular_admin;Password=Milanesa35772799;");
+            con = new SqlConnection("Data Source=SQL1002.site4now.net;Initial Catalog=db_ab28c4_basepruebaangular;User Id=db_ab28c4_basepruebaangular_admin;Password=Milanesa35772799;TrustServerCertificate=True;");
             }
 
         // METODO CONECTAR
